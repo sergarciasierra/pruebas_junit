98ba3eb8e08fd7190330f652968c89283842e8c8
sergarciasierra <sgarcia05@gmail.com>
2018-05-30 15:41:19 -0500

    Calculadora simple con @Before y @After para las pruebas

--- src/main/java/Calculadora.java
+++ src/main/java/Calculadora.java
@@ -1,12 +1,36 @@
 
 public class Calculadora {
-    
-	public static int suma(int a, int b) {
-        return a + b;
+    private int ans;
+    public Calculadora(){
+    	ans = 0;
+    }
+	public int suma(int a, int b) {
+		ans = a + b;
+        return ans;
     }
 
-	public static int resta(int a, int b) {
-        return a - b;
+	public int resta(int a, int b) {
+		ans = a -b;
+        return ans;
     }
+	
+	public int suma(int s) {
+		ans += s;
+		return ans;
+	}
+	
+	public int resta(int r) {
+		ans -= r;
+		return ans;
+	}
+	
+	public int ans() {
+		return ans;
+	}
+	
+	public int clear() {
+		ans = 0;
+		return ans;
+	}
 
 }
